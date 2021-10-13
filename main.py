fbdaa04bd0da182bce0fe5ae9dc87d186d207e86
Nicolas Kingreen <NicolasKingreen@gmail.com>
2021-10-13 16:48:06 +0300

    game engine

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import *
 
 from window import Window
-from gui import Button
+from engine import Engine
+from game_state import MenuState
 
 
 class Application:
@@ -10,46 +11,14 @@ class Application:
     MAX_FPS = 120
 
     def __init__(self):
-        self.clock = pygame.time.Clock()
-        self.window = Window()
+        self.window = Window("Test GUI")
+        self.engine = Engine(self.window)
 
-        self.start_button = Button("Start", (40, 160), (200, 50))
-        self.options_button = Button("Options", (40, 200), (200, 50))
-        self.exit_button = Button("Exit", (40, 240), (200, 50))
-        self.exit_button.set_on_click_function(self.stop)
+        self.menu_state = MenuState(self.engine)
+        self.engine.set_state(self.menu_state)
 
-        self.is_running = False
-
-    def run(self):
-        self.is_running = True
-        while self.is_running:
-
-            frame_time_ms = self.clock.tick(Application.MAX_FPS)
-            frame_time_s = frame_time_ms / 1000.
-
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    self.stop()
-                elif event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        self.stop()
-
-            self.start_button.update()
-            self.options_button.update()
-            self.exit_button.update()
-
-            render_surface = self.window.surface
-            render_surface.fill('#90E0F3')
-            self.start_button.draw(render_surface)
-            self.options_button.draw(render_surface)
-            self.exit_button.draw(render_surface)
-            pygame.display.update()
-
-    def stop(self):
-        self.is_running = False
-        pygame.quit()
-        exit()
+        self.engine.run()
 
 
 if __name__ == "__main__":
-    Application().run()
+    Application()
